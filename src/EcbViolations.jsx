5e0f997eaff4a62804fa132647f567a3f31367e0
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2023-12-12 10:52:56 -0500

    added filtering and excel export to ECB Violations

--- src/EcbViolations.jsx
+++ src/EcbViolations.jsx
@@ -1,14 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import * as XLSX from 'xlsx';
 
 function EcbViolations() {
   const [bin, setBin] = useState('');
   const [violations, setViolations] = useState([]);
+  const [filter, setFilter] = useState('all'); // Default filter: all
+  const searchClicked = useRef(false);
 
   const fetchData = async () => {
-    if (!bin) return;
+    if (!bin || !searchClicked.current) return;
 
     try {
-      const url = `https://data.cityofnewyork.us/resource/6bgk-3dad.json?bin=${bin}`;
+      let url = `https://data.cityofnewyork.us/resource/6bgk-3dad.json?bin=${bin}`;
+
+      // Add filtering based on the selected option
+      if (filter !== 'all') {
+        url += `&ecb_violation_status=${filter}`;
+      }
+
       const response = await fetch(url);
       const data = await response.json();
       setViolations(data);
@@ -19,7 +28,29 @@ function EcbViolations() {
 
   useEffect(() => {
     fetchData();
-  }, [bin]);
+  }, [bin, filter, searchClicked]);
+
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
+  };
+
+  const handleSearch = () => {
+    searchClicked.current = true;
+    fetchData();
+  };
+  
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      handleSearch();
+    }
+  };
+
+  const exportToExcel = () => {
+    const ws = XLSX.utils.json_to_sheet(violations);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Violations');
+    XLSX.writeFile(wb, 'ecb_violations.xlsx');
+  };
 
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
@@ -32,12 +63,61 @@ function EcbViolations() {
               type="text"
               value={bin}
               onChange={(e) => setBin(e.target.value)}
+              onKeyPress={handleKeyPress}
               className="ml-2 border border-gray-300 px-3 py-2 rounded focus:outline-none focus:border-blue-500"
             />
           </label>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded"
+            onClick={handleSearch}
+          >
+            Search
+          </button>
         </div>
+        <div className="mb-4">
+          <label className="mr-4">Filter By:</label>
+          <div className="flex space-x-4">
+            <button
+              className={`${
+                filter === 'all' ? 'bg-blue-500 text-white' : 'bg-gray-300 text-gray-800'
+              } px-4 py-2 rounded`}
+              onClick={() => handleFilterChange('all')}
+            >
+              All
+            </button>
+            <button
+              className={`${
+                filter === 'ACTIVE'
+                  ? 'bg-blue-500 text-white'
+                  : 'bg-gray-300 text-gray-800'
+              } px-4 py-2 rounded`}
+              onClick={() => handleFilterChange('ACTIVE')}
+            >
+              Active Only
+            </button>
+            <button
+              className={`${
+                filter === ('RESOLVE')
+                  ? 'bg-blue-500 text-white'
+                  : 'bg-gray-300 text-gray-800'
+              } px-4 py-2 rounded`}
+              onClick={() => handleFilterChange('RESOLVE')}
+            >
+              Resolved/Dismissed
+            </button>
+          </div>
+        </div>
+        <p className="text-lg font-bold mb-4">
+          Violation Count: {violations.length}
+        </p>
+        <button
+          className="bg-green-500 text-white mb-5 px-4 py-2 rounded"
+          onClick={exportToExcel}
+        >
+          Export to Excel
+        </button>
         <ul>
-          {violations.map((violation) => (
+        {violations.map((violation) => (
             <li
               key={violation.ecb_violation_number}
               className="bg-gray-200 p-4 mb-4 rounded shadow-md"
