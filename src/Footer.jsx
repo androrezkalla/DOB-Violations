524b6990905ab82fd1bc257778e2cd11a8b31e52
Andro Rezkalla <78698964+androrezkalla@users.noreply.github.com>
2024-04-25 14:10:38 -0400

    updated footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white p-4 text-center">
-      <p>&copy; Andro Rezkalla 2023. All rights reserved.</p>
+      <p>&copy; Andro Rezkalla 2024. All rights reserved.</p>
     </footer>
   );
 };
